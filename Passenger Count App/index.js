10a288bce86ae08d3174f427db3c1575d9994f56
Fazalparkar <77339626+Fazalparkar@users.noreply.github.com>
2023-11-11 15:54:51 +0530

    Add files via upload

---
+++ Passenger Count App/index.js
@@ -0,0 +1,34 @@
+// document.getElementById("count").innerText = 5
+// intialize the count as 0
+// listen for clicks on the increment button
+
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
+console.log(countEl);
+let count = 0;
+
+function increment() {
+    count += 1;
+    countEl.textContent = count;
+}
+
+function decrement() {
+    count -= 1;
+    countEl.textContent = count;
+}
+
+function save() {
+    let countStr = count + "-";
+    saveEl.textContent += countStr;
+    countEl.textContent = 0;
+    count = 0;
+}
+
+
+
+
+
+
+
+
+
